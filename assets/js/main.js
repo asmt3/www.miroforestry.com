97e6dd7b520366a093fb8ca5766654c97066e7dd
Alan Thomson <alan@projectmccoll.co.uk>
2017-08-30 21:50:26 +0100

    Final changes pre Andrew

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,19 @@
 $(function(){
   $("nav ul>li>a").click(function(e){
     var isMobile = $('.menu-icon').css('display')=='block';
-    var noChildren = $(this).hasClass('no-children');
+    var hasChildren = $(this).hasClass('has-children');
 
-    if (noChildren || !isMobile) {
-      // do not intefere
-      // allow click to proceed
-    } else {
+    if ( hasChildren && isMobile) {
       // has children AND on mobile: show submenu on click
       var thisSubmenu = $(this).siblings('ul')
       var allSubmenus = $("nav ul ul")
       allSubmenus.hide();
       thisSubmenu.show();
       e.preventDefault();
+      console.log('prevent')
     }
+    // else: 
+    // do not intefere
+    // allow click to proceed
   })
 })
